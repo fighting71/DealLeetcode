e1002ddfdcd9aafd17f47e5ba6b965b1cda4a3e3
fighting71 <35062179+monster71@users.noreply.github.com>
2020-10-23 18:29:19 +0800

    sync

--- src/Questions/Questions.DailyChallenge/2020/October/Week3/Search a 2D Matrix.cs
+++ src/Questions/Questions.DailyChallenge/2020/October/Week3/Search a 2D Matrix.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Questions.DailyChallenge._2020.October.Week2
+namespace Questions.DailyChallenge._2020.October.Week3
 {
 
     /// <summary>
---
+++ src/Questions/Questions.DailyChallenge/2020/October/Week4/Minimum Depth of Binary Tree.cs
@@ -0,0 +1,76 @@
+﻿using Command.CommonStruct;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questions.DailyChallenge._2020.October.Week4
+{
+
+    /// <summary>
+    /// @auth : monster
+    /// @since : 10/23/2020 10:20:46 AM
+    /// @source : https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/562/week-4-october-22nd-october-28th/3504/
+    /// @des : 
+    /// </summary>
+    public class Minimum_Depth_of_Binary_Tree
+    {
+
+        //52 / 52 test cases passed.
+        //Status: Accepted
+        //Runtime: 264 ms
+        //Memory Usage: 50.9 MB
+        // 100%?
+        public int Optimize(TreeNode root)
+        {
+
+            if (root == null) return 0;
+
+            Queue<(TreeNode, int)> queue = new Queue<(TreeNode, int)>();
+            queue.Enqueue((root, 1));
+
+            while (queue.Count > 0)
+            {
+                (TreeNode node, int deep) = queue.Dequeue();
+
+                bool hasLeft = node.left != null, hasRight = node.right != null;
+                if (!hasLeft && !hasRight) return deep;
+                if (hasLeft)
+                    queue.Enqueue((node.left, deep + 1));
+                if (hasRight)
+                    queue.Enqueue((node.right, deep + 1));
+
+            }
+            return default;
+            
+            //Queue<TreeNode> queue = new Queue<TreeNode>();
+            //queue.Enqueue(root);
+
+            //while (queue.Count > 0)
+            //{
+            //    TreeNode node = queue.Dequeue();
+
+            //    bool hasLeft = node.left != null, hasRight = node.right != null;
+            //    if (!hasLeft && !hasRight) return 0;
+            //    if (hasLeft)
+            //        queue.Enqueue(node.left);
+            //    if (hasRight)
+            //        queue.Enqueue(node.right);
+
+            //}
+
+        }
+
+        public int Simple(TreeNode root)
+        {
+            return Help(root, 1);
+        }
+
+        private int Help(TreeNode node, int deep)
+        {
+            if (node == null) return deep - 1;
+            return Math.Min(Help(node.left, deep + 1),
+                Help(node.right, deep + 1));
+        }
+
+    }
+}
