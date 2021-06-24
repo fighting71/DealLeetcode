e28a8fdbffc1f4f0fd909a9fc5abc5525f69101d
fighting71 <35062179+fighting71@users.noreply.github.com>
2021-06-24 19:02:45 +0800

    async

--- src/Questions/Console.Test/Program.cs
+++ src/Questions/Console.Test/Program.cs
@@ -100,25 +100,6 @@ namespace ConsoleTest
             bool runSimple = true;
             runSimple = false;
 
-            {
-                Out_of_Boundary_Paths instance = new Out_of_Boundary_Paths();
-
-                BaseLibrary.CommonTest(new[] {
-                    (2,2,2,0,0),//6
-                    (1,3,3,0,1),//12
-                }
-                , arg => instance.FindPaths(arg.Item1, arg.Item2, arg.Item3, arg.Item4, arg.Item5)
-                , () =>
-                {
-                    //int m = 50, n = 50;
-                    int m = random.Next(50)+1, n = random.Next(50) + 1;
-
-                    return (m, n, 50, random.Next(m), random.Next(n));
-                }
-                );
-
-            }
-
             { if (runSimple) { } else { } }
 
             Console.WriteLine("Hello World!");
